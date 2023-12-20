18cdff54a1db3070fc47fdd4cb0839cdd83e9df4
shihab01118 <shahed.shihab.0@gmail.com>
2023-12-21 04:50:04 +0600

    functionality for signup page added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes.jsx";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <Toaster />
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,22 +1,53 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaLock, FaUser } from "react-icons/fa6";
 import { TbMailFilled } from "react-icons/tb";
 import { BiSolidUserAccount } from "react-icons/bi";
 import signup_image from "../../assets/images/signup.jpg";
+import useAuth from "../../hooks/useAuth";
+import { Helmet } from "react-helmet-async";
+import toast from "react-hot-toast";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const { createUser, updateUserProfile } = useAuth();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [imgURL, setImgURL] = useState("");
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
+
+    // password validation
+    if (
+      !/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\-]).{6,}$/.test(password)
+    ) {
+      toast.error(
+        "Password must contain 6 characters, one capital letter and a special character."
+      );
+      return;
+    }
+
+    // create user
+    const result = await createUser(email, password);
+
+    // update user profile
+    await updateUserProfile(name, imgURL);
+
+    // navigate user after signup
+    if (result?.user) {
+      navigate("/dashboard");
+      toast.success("Registration Successful!");
+    }
   };
 
   return (
     <>
+      <Helmet>
+        <title>Taskify - Sign Up</title>
+      </Helmet>
       <div className="flex min-h-screen items-center">
         <div className="max-w-4xl w-full mx-8 md:mx-16 lg:mx-auto bg-base-100 rounded-xl shadow-xl p-10 md:p-16 flex flex-col-reverse lg:flex-row md:gap-8 lg:gap-20">
           <div className="flex-1">
