80d19886241ff7ef8d4b15079da9459d0b6ba776
shihab01118 <shahed.shihab.0@gmail.com>
2023-12-22 15:36:18 +0600

    slides added to demographic items

--- src/pages/Home/DemoGraphicUsers/DemoGraphicUsers.jsx
+++ src/pages/Home/DemoGraphicUsers/DemoGraphicUsers.jsx
@@ -1,6 +1,12 @@
 import Chart from "react-apexcharts";
+import { Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const DemoGraphicUsers = () => {
+  // chart data
   const chartData = {
     series: [40, 25, 20, 15], // Sample percentage breakdown
     options: {
@@ -36,34 +42,70 @@ const DemoGraphicUsers = () => {
     },
   };
 
+  //   demographic data
+  const demographicItems = [
+    {
+      title: "Developers",
+      description: "40% of our users are developers who benefit from Taskify!",
+    },
+    {
+      title: "Project Managers",
+      description:
+        "25% of our users are project managers who benefit from Taskify!",
+    },
+    {
+      title: "Students",
+      description: "20% of our users are students who benefit from Taskify!",
+    },
+    {
+      title: "Freelancers",
+      description: "15% of our users are freelancers who benefit from Taskify!",
+    },
+  ];
+
   return (
     <div className="max-w-6xl mx-8 md:mx-16 lg:mx-auto my-20">
-      <h2 className="text-3xl font-semibold mb-6 text-center">
+      <h2 className="text-2xl md:text-3xl font-semibold mb-6 text-center">
         Who Benefits from Our Platform?
       </h2>
-      <div className="mb-8">
-        {/* ApexCharts pie chart */}
+      <div className="mb-8 w-full md:w-1/2 mx-auto">
         <Chart
           options={chartData.options}
           series={chartData.series}
           type="pie"
-          width="50%"
+          width="100%"
         />
       </div>
-      {/* Demographic items */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="p-4 bg-white rounded-md shadow-md">
-          <h3 className="text-lg font-semibold mb-2">Developers</h3>
-          <p>40% of our users are developers who benefit from...</p>
-        </div>
-        <div className="p-4 bg-white rounded-md shadow-md">
-          <h3 className="text-lg font-semibold mb-2">Project Managers</h3>
-          <p>25% of our users are project managers who benefit from...</p>
-        </div>
-        <div className="p-4 bg-white rounded-md shadow-md">
-          <h3 className="text-lg font-semibold mb-2">Students</h3>
-          <p>20% of our users are students who benefit from...</p>
-        </div>
+      <div className="swiper-container">
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{ clickable: true }}
+          autoplay
+          breakpoints={{
+            1024: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 2, // Show 2 items on tablets
+            },
+            480: {
+              slidesPerView: 1, // Show 1 item on mobile
+            },
+          }}
+        >
+          {demographicItems.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="p-4 bg-gray-200 rounded-md shadow-md">
+                <h3 className="text-lg font-semibold mb-2">{item.title}</h3>
+                <p className="text-[#757575] text-sm font-medium">
+                  {item.description}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
