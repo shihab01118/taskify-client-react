4b79fdb56c43a7ef6462b4f1672ca6d03896ebb3
shihab01118 <shahed.shihab.0@gmail.com>
2023-12-22 11:57:58 +0600

    animation added to banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,12 +2,24 @@ import Lottie from "lottie-react";
 import bannerAnimation from "../../../assets/animations/banner-animation.json";
 import { FaArrowTrendUp } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Banner = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+    });
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center bg-gradient-to-br from-[#027970b3] to-[#68cc82b3] ">
       <div className="max-w-6xl mx-8 md:mx-16 lg:mx-auto flex flex-col lg:flex-row pt-[130px] text-center lg:text-left lg:pt-0 justify-between items-center">
-        <div className="space-y-5 max-w-xl">
+        <div data-aos="fade-up" className="space-y-5 max-w-xl">
           <h2 className="text-4xl md:text-5xl lg:text-6xl font-semibold text-title">
             Gear Your Wheel
           </h2>
@@ -23,9 +35,16 @@ const Banner = () => {
             </Link>
           </div>
         </div>
-        <div className="md:w-3/5 lg:w-2/5">
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{
+            duration: .8,
+          }}
+          className="md:w-3/5 lg:w-2/5"
+        >
           <Lottie animationData={bannerAnimation}></Lottie>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
